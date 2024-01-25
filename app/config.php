31e68d0e446bd35737fece077c4dcbe690f43ac2
HEL70N <heltonrodolfolourenco.01@gmail.com>
2024-01-25 13:19:03 +0200

    first commit

---
+++ app/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Para saber se estamos no servidor local
+define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
+define('URL', (IS_LOCAL ? 'http://127.0.0.1/cotizador' : 'URL DE SERVIDOR EM PORDUÇÃO'));
+
+// Rotas das pastas
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', getcwd() . DS);
+define('APP', ROOT . 'app' . DS);
+define('ASSETS', ROOT . 'assets' . DS);
+define('TEMPLATES', ROOT . 'templates' . DS);
+define('INCLUDES', TEMPLATES . 'includes' . DS);
+define('MODULES', TEMPLATES . 'modules' . DS);
+define('VIEWS', TEMPLATES . 'views' . DS);
+define('UPLOADS', ROOT . 'uploads' . DS);
+
+// Para arquivos que vamos incluir no cabeçalho e rodapé (CSS e JS)
+define('CSS', URL . 'assets/css/');
+define('IMG', URL . 'assets/img/');
+define('JS', URL . 'assets/js/');
