3f09659c6f86ed35a8cb02ad3a73d0add1900b70
HEL70N <heltonrodolfolourenco.01@gmail.com>
2024-01-25 16:15:10 +0200

    Criação da área do formulário geral

--- templates/views/indexView.php
+++ templates/views/indexView.php
@@ -42,13 +42,84 @@
     </header>
     <!-- Fim da NavBar -->
 
+    <!-- Content -->
+    <div class="container-fluid py-5">
+        <div class="row">
+            <div class="col-lg-8 col-12">
+                <div class="card mb-3">
+                    <div class="card-header">Informação de Cliente</div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group row">
+                                <div class="col-4">
+                                    <label for="nome">Nome</label>
+                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Helton Rodolfo" required>
+                                </div>
+                                <div class="col-4">
+                                    <label for="empresa">Empresa</label>
+                                    <input type="text" class="form-control" id="empresa" name="empresa" placeholder="Acliven Su" required>
+                                </div>
+                                <div class="col-4">
+                                    <label for="email">E-mail</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="heltonrodolfo.it@gmail.com" required>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">Adicionar Novo Produto</div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group row">
+                                <div class="col-3">
+                                    <label for="produto">Produto</label>
+                                    <input type="text" class="form-control" id="produto" name="produto" placeholder="Guitar Eletrica" required>
+                                </div>
+                                <div class="col-3">
+                                    <label for="tipo">Tipo de Produto</label>
+                                    <select name="tipo" id="tipo" class="form-control">
+                                        <option value="produto">Produto</option>
+                                        <option value="servico">Serviço</option>
+                                    </select>
+                                </div>
+                                <div class="col-3">
+                                    <label for="quantidade">Quantidade</label>
+                                    <input type="number" class="form-control" id="quantidade" name="quantidade" min="1" max="99999" required>
+                                </div>
+                                <div class="col-3">
+                                    <label for="precoUnitario">Preço Unitário</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">MZN</span>
+                                        </div>
+                                        <input type="number" class="form-control" id="precoUnitario" name="precoUnitario" min="1" max="99999" placeholder="0.0" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <button class="btn btn-success" type="submit">Salvar</button>
+                            <button class="btn btn-danger" type="reset">Cancelar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-12">
+
+            </div>
+        </div>
+    </div>
+    <!-- End content -->
+
     <!-- Rodapé -->
     <footer class="bg-light text-muted py-5">
         <div class="container">
             <p class="float-right">
                 <a href="#">Topo</a>
             </p>
-            <p><a href="#">Cotizador App</a> &copy; Todos direitos reservados <?php echo date('Y'); ?></p>
+            <p><a href="#">HE L T ON</a> &copy; Todos direitos reservados <?php echo date('Y'); ?></p>
         </div>
     </footer>
 
