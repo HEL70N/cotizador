f24b7cced8960ff04400268df4a121fa1a6f63af
HEL70N <heltonrodolfolourenco.01@gmail.com>
2024-01-25 15:12:30 +0200

    Criação de Rodapé

--- templates/views/indexView.php
+++ templates/views/indexView.php
@@ -40,6 +40,17 @@
             </div>
         </div>
     </header>
+    <!-- Fim da NavBar -->
+
+    <!-- Rodapé -->
+    <footer class="bg-light text-muted py-5">
+        <div class="container">
+            <p class="float-right">
+                <a href="#">Topo</a>
+            </p>
+            <p><a href="#">Cotizador App</a> &copy; Todos direitos reservados <?php echo date('Y'); ?></p>
+        </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
