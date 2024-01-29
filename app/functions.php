cfb997d5c863bc777a99c38cf5c0fa3db83076f7
HEL70N <heltonrodolfolourenco.01@gmail.com>
2024-01-29 12:04:34 +0200

    Funções jsonBuild & jsonOutput

--- app/functions.php
+++ app/functions.php
@@ -172,3 +172,57 @@ function addItem($item)
     $_SESSION['newQuote']['itens'][] = $item;
     return true;
 }
+
+function jsonBuild($status = 200, $data = null, $msg = '')
+{
+    if (empty($msg) || $msg == '') {
+        switch ($status) {
+            case 200:
+                $msg = 'OK';
+                break;
+            case 201:
+                $msg = 'Created';
+                break;
+            case 400:
+                $msg = 'Invalid request';
+                break;
+            case 403:
+                $msg = 'Acess denied';
+                break;
+            case 404:
+                $msg = 'Not found';
+                break;
+            case 500:
+                $msg = 'Interal Server Error';
+                break;
+            case 550:
+                $msg = 'Permission denied';
+                break;
+            default:
+                break;
+        }
+    }
+
+    $json =
+        [
+            'status' => $status,
+            'data' => $data,
+            'msg' => $msg
+        ];
+
+    return json_encode($json);
+}
+
+function jsonOutput($json)
+{
+    header('Access-Central-Allow-Origin: *');
+    header('Content-type: application/json;charset=utf-8');
+
+    if (is_array($json)) {
+        $json = json_encode($json);
+    }
+
+    echo $json;
+
+    return true;
+}
